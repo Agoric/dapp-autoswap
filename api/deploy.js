57b8fbf93f4b50a2e7a1b6db285ab24a9f58fad4
Michael FIG <mfig@agoric.com>
2020-03-04 12:13:57 -0600

    fix: add the API handler (taken from ag-solo/vats/lib-exchange.js)

--- api/deploy.js
+++ api/deploy.js
@@ -1,8 +1,9 @@
 // Agoric Dapp api deployment script
 
 export default async function deployApi(homeP, { bundleSource, pathResolve }) {
-  const { source, moduleFormat } = await bundleSource('./handler.js');
+  const { source, moduleFormat } = await bundleSource(pathResolve('./handler.js'));
   const handlerInstall = homeP~.spawner~.install(source, moduleFormat);
-  const handler = handlerInstall~.spawn();
+  const [zoe, registrar] = await Promise.all([homeP~.zoe, homeP~.registrar]);
+  const handler = handlerInstall~.spawn({zoe, registrar});
   await homeP~.http~.registerCommandHandler(handler);
 }
--- api/handler.js
+++ api/handler.js
@@ -1,16 +1,123 @@
 import harden from '@agoric/harden';
 
-export default harden((_terms, _inviteMaker) => {
+function checkOrder(a0, a1, b0, b1) {
+  if (a0 === b0 && a1 === b1) {
+    return true;
+  }
+
+  if (a0 === b1 && a1 === b0) {
+    return false;
+  }
+
+  throw new TypeError('Canot resolve asset ordering');
+}
+
+export default harden(({zoe, registrar}, _inviteMaker) => {
+  function getPrice(instanceId, extent0, assayId0, _assayId1) {
+    const instanceHandleP = E(registrar).get(instanceId);
+    const regAssay0P = E(registrar).get(assayId0);
+    const label0P = E(regAssay0P).getLabel();
+
+    return Promise.all([instanceHandleP, label0P]).then(
+      ([instanceHandle, label0]) =>
+        E(zoe)
+          .getInstance(instanceHandle)
+          .then(({ publicAPI }) => {
+            const unit0 = harden({ label: label0, extent: extent0 });
+
+            return E(publicAPI)
+              .getPrice(unit0)
+              .then(unit1 => unit1.extent);
+          }),
+    );
+  }
+
+  function getOfferRules(instanceId, extent0, assayId0, assayId1) {
+    const instanceHandleP = E(registrar).get(instanceId);
+    const regAssay0P = E(registrar).get(assayId0);
+    const regAssay1P = E(registrar).get(assayId1);
+
+    return Promise.all([instanceHandleP, regAssay0P, regAssay1P]).then(
+      ([instanceHandle, regAssay0, regAssay1]) =>
+        E(zoe)
+          .getInstance(instanceHandle)
+          .then(
+            ({
+              terms: {
+                assays: [contractAssay0, contractAssay1],
+              },
+            }) => {
+              // Check whether we sell on contract assay 0 or 1.
+              const normal = checkOrder(
+                regAssay0,
+                regAssay1,
+                contractAssay0,
+                contractAssay1,
+              );
+
+              // Construct the rules for serialization (no instance).
+              // This rule is the payment
+              const payinRule = {
+                kind: 'offerAtMost',
+                units: { assayId: assayId0, extent: extent0 },
+              };
+              // This rule is the payout
+              const payoutRule = {
+                kind: 'wantAtLeast',
+                units: { assayId: assayId1 },
+              };
+
+              // Order the rules accordingly.
+              const offerRules = harden({
+                payoutRules: [
+                  normal ? payinRule : payoutRule,
+                  normal ? payoutRule : payinRule,
+                  {
+                    kind: 'wantAtLeast',
+                    units: {},
+                  },
+                ],
+                exitRule: {
+                  kind: 'onDemand',
+                },
+              });
+
+              return offerRules;
+            },
+          ),
+    );
+  }
+
   return harden({
     getCommandHandler() {
       return harden({
-        processInbound(obj, _home) {
-          switch (obj.type) {
-            case 'dapp-autoswapMessage': {
-              return harden({ type: 'dapp-autoswapResponse', orig: obj });
+        async processInbound(obj, _home) {
+          const { type, data } = obj;
+          switch (type) {
+            case 'autoswapGetPrice': {
+              const { instanceId, extent0, assayId0, assayId1 } = data;
+              const extent = await getPrice(
+                instanceId,
+                extent0,
+                assayId0,
+                assayId1,
+              );
+              return { type: 'autoswapPrice', data: extent };
             }
-            default:
-              return undefined;
+  
+            case 'autoswapGetOfferRules': {
+              const { instanceId, extent0, assayId0, assayId1 } = data;
+              const offerRules = await getOfferRules(
+                instanceId,
+                extent0,
+                assayId0,
+                assayId1,
+              );
+              return { type: 'autoswapOfferRules', data: offerRules };
+            }
+            default: {
+              return false;
+            }
           }
         },
       });
