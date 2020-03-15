00d1914c4e6ae4b207464397ba235ee6bb11e8cb
Michael FIG <mfig@agoric.com>
2020-03-15 17:14:42 -0600

    refactor: separate autoswap-specific initialization
    Now contract/install-autoswap.js uses the wallet's offer mechanism
    to instantiate the contract and add liquidity.

--- contract/deploy.js
+++ contract/deploy.js
@@ -1,177 +1,53 @@
-// Agoric Dapp contract deployment script for autoswap
+// Generic Agoric Dapp contract deployment script
+// NOTE: YOUR CONTRACT-SPECIFIC INITIALIZATION is in install-*.js
 import fs from 'fs';
-
-import harden from '@agoric/harden';
-import makeAmountMath from '@agoric/ertp/src/amountMath';
 
 // This javascript source file uses the "tildot" syntax (foo~.bar()) for
 // eventual sends. Tildot is standards track with TC39, the JavaScript standards
 // committee.
 // TODO: improve this comment. https://github.com/Agoric/agoric-sdk/issues/608
 
-const DAPP_NAME = "dapp-autoswap";
+const DAPP_NAME = "autoswap";
 
 export default async function deployContract(homeP, { bundleSource, pathResolve },
-  CONTRACT_NAME = 'autoswap') {
+  CONTRACT_NAME = DAPP_NAME) {
+
+  const [
+    { source, moduleFormat },
+    contractBundle,
+  ] = await Promise.all([
+    bundleSource(pathResolve(`./install-${CONTRACT_NAME}.js`)),
+    bundleSource(pathResolve(`./${CONTRACT_NAME}.js`)),
+  ]);
 
   const wallet = homeP~.wallet;
+  const zoe = homeP~.zoe;
+  const registrar = homeP~.registrar;
 
-  // Create a source bundle for the "myFirstDapp" smart contract.
-  const { source, moduleFormat } = await bundleSource(pathResolve(`./${CONTRACT_NAME}.js`));
+  const installerInstall = homeP~.spawner~.install(source, moduleFormat);
+  const installer = installerInstall~.spawn({ wallet, zoe, registrar });
 
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
+  const { instanceId } = await installer~.initInstance(CONTRACT_NAME, contractBundle);
 
-  const installationHandle = await homeP~.zoe~.install(source, moduleFormat);
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  // 1. Issuers and payments
-  const [[pursePetname0, purse0], [pursePetname1, purse1]] = await wallet~.getPurses();
-  const issuer0P = wallet~.getPurseIssuer(pursePetname0);
-  const issuer1P = wallet~.getPurseIssuer(pursePetname1);
-
-  const getLocalAmountMath = issuer =>
-    Promise.all([
-      issuer~.getBrand(),
-      issuer~.getMathHelpersName(),
-  ]).then(([brand, mathHelpersName]) => makeAmountMath(brand, mathHelpersName));
-
-  const amountMath0P = getLocalAmountMath(issuer0P);
-  const amountMath1P = getLocalAmountMath(issuer1P);
-  const withdrawAmount = (amountMath, purse, extent) =>
-    amountMath.then(am => purse~.withdraw(am.make(extent)));
-
-  const extent0 = 900;
-  const extent1 = 500;
-  const payment0P = withdrawAmount(amountMath0P, purse0, extent0);
-  const payment1P = withdrawAmount(amountMath1P, purse1, extent1);
-
-  const [
-    issuer0,
-    issuer1,
-    amountMath0,
-    amountMath1,
-    payment0,
-    payment1
-  ] = await Promise.all([
-    issuer0P,
-    issuer1P,
-    amountMath0P,
-    amountMath1P,
-    payment0P,
-    payment1P
-  ]);
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  // 2. Contract instance.
-  const [
-    invite,
-    inviteIssuer,
-  ] = await Promise.all([
-    homeP~.zoe~.makeInstance(installationHandle, { issuers: [issuer0, issuer1] }),
-    homeP~.zoe~.getInviteIssuer(),
-  ])
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  // 3. Get the instanceHandle
-
-  const {
-    extent: [{ instanceHandle }],
-  } = await inviteIssuer~.getAmountOf(invite);
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  // 4. Get the contract terms and issuers
-
-  const { terms: { issuers }} = await homeP~.zoe~.getInstance(instanceHandle);
-
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  // 5. Offer rules
-  const amountMath2 = await getLocalAmountMath(issuers[2])
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  const offerRules = harden({
-    payoutRules: [
-      {
-        kind: 'offerAtMost',
-        amount: amountMath0.make(extent0),
-      },
-      {
-        kind: 'offerAtMost',
-        amount: amountMath1.make(extent1),
-      },
-      {
-        kind: 'wantAtLeast',
-        amount: amountMath2.getEmpty(),
-      },
-    ],
-    exitRule: {
-      kind: 'onDemand',
-    },
-  });
-
-  // 6. Liquidities.
-
-  const payments = [payment0, payment1];
-
-  const { seat, payout } = await homeP~.zoe~.redeem(invite, offerRules, payments);
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  const [liquidityOk, contractId, instanceId] = await Promise.all([
-    seat~.addLiquidity(),
-    homeP~.registrar~.register(DAPP_NAME, installationHandle),
-    homeP~.registrar~.register(CONTRACT_NAME, instanceHandle),
-  ]);
-
-  // =====================
-  // === AWAITING TURN ===
-  // =====================
-
-  console.log('- installation made', CONTRACT_NAME, '=>',  contractId);
   console.log('- instance made', CONTRACT_NAME, '=>', instanceId);
-  console.log(liquidityOk);
 
   // Save the instanceId somewhere where the UI can find it.
-  if (liquidityOk) {
-    const dappConstants = {
-      BRIDGE_URL: 'agoric-lookup:https://local.agoric.com?append=/bridge',
-      API_URL: '/',
-      CONTRACT_ID: instanceId,
-    };
-    const dc = 'dappConstants.js';
-    console.log('writing', 'dappConstants.js');
-    await fs.promises.writeFile(dc, `globalThis.__DAPP_CONSTANTS__ = ${JSON.stringify(dappConstants, undefined, 2)}`);
+  const dappConstants = {
+    BRIDGE_URL: 'agoric-lookup:https://local.agoric.com?append=/bridge',
+    API_URL: '/',
+    CONTRACT_ID: instanceId,
+  };
+  const dc = 'dappConstants.js';
+  console.log('writing', dc);
+  await fs.promises.writeFile(dc, `globalThis.__DAPP_CONSTANTS__ = ${JSON.stringify(dappConstants, undefined, 2)}`);
 
-    // Now add URLs so that development functions without internet access.
-    dappConstants.BRIDGE_URL = "http://127.0.0.1:8000";
-    dappConstants.API_URL = "http://127.0.0.1:8000";
-    const envFile = pathResolve(`../ui/.env.local`);
-    console.log('writing', envFile);
-    const envContents = `\
-  REACT_APP_DAPP_CONSTANTS_JSON='${JSON.stringify(dappConstants)}'
+  // Now add URLs so that development functions without internet access.
+  dappConstants.BRIDGE_URL = "http://127.0.0.1:8000";
+  dappConstants.API_URL = "http://127.0.0.1:8000";
+  const envFile = pathResolve(`../ui/.env.local`);
+  console.log('writing', envFile);
+  const envContents = `\
+REACT_APP_DAPP_CONSTANTS_JSON='${JSON.stringify(dappConstants)}'
 `;
-    await fs.promises.writeFile(envFile, envContents);
-  }
+  await fs.promises.writeFile(envFile, envContents);
 }
---
+++ contract/install-autoswap.js
@@ -0,0 +1,108 @@
+import harden from '@agoric/harden';
+
+// This initInstance function is specific to autoswap.
+//
+// Notably, it takes the first two purses of the wallet and
+// uses them to add liquidity.
+export default harden(({ wallet, zoe, registrar }) => {
+  return harden({
+    async initInstance(contractName, { source, moduleFormat }) {
+      const installationHandle = await zoe~.install(source, moduleFormat);
+
+      // =====================
+      // === AWAITING TURN ===
+      // =====================
+    
+      // 1. Issuers and purse petnames
+      // Just take the first two purses.
+      const [[pursePetname0], [pursePetname1]] = await wallet~.getPurses()
+
+      // =====================
+      // === AWAITING TURN ===
+      // =====================
+    
+      const [issuer0, issuer1] = await Promise.all([
+        wallet~.getPurseIssuer(pursePetname0),
+        wallet~.getPurseIssuer(pursePetname1),
+      ]);
+
+      // =====================
+      // === AWAITING TURN ===
+      // =====================
+    
+      // 2. Contract instance.
+      const [
+        invite,
+        inviteIssuer,
+      ] = await Promise.all([
+        zoe~.makeInstance(installationHandle, { issuers: [issuer0, issuer1] }),
+        zoe~.getInviteIssuer(),
+      ])
+    
+      // =====================
+      // === AWAITING TURN ===
+      // =====================
+    
+      // 3. Get the instanceHandle
+    
+      const {
+        extent: [{ instanceHandle }],
+      } = await inviteIssuer~.getAmountOf(invite);
+      const instanceId = await registrar~.register(contractName, instanceHandle);
+    
+      const extent0 = 900;
+      const extent1 = 500;
+    
+      const offerDesc = {
+        id: Date.now(),
+    
+        // Contract-specific metadata.
+        instanceRegKey: instanceId,
+        contractIssuerIndexToRole: ['TokenA', 'TokenB', 'Liquidity'],
+    
+        offerRulesTemplate: {
+          offer: {
+            TokenA: {
+              pursePetname: pursePetname0,
+              extent: extent0,
+            },
+            TokenB: {
+              pursePetname: pursePetname1,
+              extent: extent1,
+            },
+          },
+          exit: { onDemand: {} },
+        },
+      };
+    
+      let resolve;
+      let reject;
+      const acceptedP = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      const hooks = harden({
+        publicAPI: {
+          getInvite(_publicAPI) {
+            return invite;
+          },
+          offerAccepted(_publicAPI) {
+            resolve({ instanceId });
+          },
+        },
+        seat: {
+          performOffer(seat) {
+            return seat~.addLiquidity().catch(e => reject(`Cannot add liquidity: ${e}`));
+          },
+        },
+      });
+
+      // Use the wallet's offer system to finish the deployment.
+      const requestContext = { origin: 'autoswap deploy', date: Date.now() };
+      await wallet~.addOffer(offerDesc, hooks, requestContext);
+      wallet~.acceptOffer(offerDesc.id);
+
+      return acceptedP;
+    },
+  });
+});
